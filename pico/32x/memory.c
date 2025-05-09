saulfabregwiivc/PicoDrive

commit 2f95b8124534657b491a14d1dc83490d82908fcf

32x, improve cpu synchronisation

pico/32x/memory.c
@@ -184,7 +184,7 @@ static NOINLINE void sh2s_sync_on_read(SH2 *sh2, unsigned cycles)
 // This is used to correctly deliver syncronisation data to the 3 cpus. The
 // fifo stores 16 bit values, 8/32 bit accesses must be adapted accordingly.
 #define PFIFO_SZ	4
-#define PFIFO_CNT	16
+#define PFIFO_CNT	8
 struct sh2_poll_fifo {
   u32 cycles;
   u32 a;
@@ -947,8 +947,8 @@ static void p32x_sh2reg_write16(u32 a, u32 d, SH2 *sh2)
       Pico32x.sh2irqi[sh2->is_slave] &= ~P32XI_HINT;
       goto irls;
     case 0x1a/2:
-      Pico32x.regs[2 / 2] &= ~(1 << sh2->is_slave);
       p32x_sync_other_sh2(sh2, sh2_cycles_done_m68k(sh2));
+      Pico32x.regs[2 / 2] &= ~(1 << sh2->is_slave);
       p32x_update_cmd_irq(sh2, 0);
       return;
     case 0x1c/2:
@@ -2539,7 +2539,8 @@ void Pico32xMemStateLoaded(void)
   ssh2.poll_addr = ssh2.poll_cycles = ssh2.poll_cnt = 0;
   memset(sh2_poll_fifo, 0, sizeof(sh2_poll_fifo));
 
-  sh2_drc_flush_all();
+  if (Pico32x.emu_flags & P32XF_DRC_ROM_C)
+    sh2_drc_flush_all();
 }
 
 // vim:shiftwidth=2:ts=2:expandtab

pico/32x/32x.c
@@ -219,6 +219,7 @@ void Pico32xInit(void)
 void PicoPower32x(void)
 {
   memset(&Pico32x, 0, sizeof(Pico32x));
+  memset(p32x_event_times, 0, sizeof(p32x_event_times));
 
   Pico32x.regs[0] = P32XS_REN|P32XS_nRES; // verified
   Pico32x.regs[0x10/2] = 0xffff;
@@ -241,10 +242,10 @@ void PicoUnload32x(void)
 void PicoReset32x(void)
 {
   if (PicoIn.AHW & PAHW_32X) {
-    p32x_trigger_irq(NULL, SekCyclesDone(), P32XI_VRES);
+    p32x_trigger_irq(NULL, Pico.t.m68c_aim, P32XI_VRES);
     p32x_m68k_poll_event(0, -1);
-    p32x_sh2_poll_event(msh2.poll_addr, &msh2, SH2_IDLE_STATES, SekCyclesDone());
-    p32x_sh2_poll_event(ssh2.poll_addr, &ssh2, SH2_IDLE_STATES, SekCyclesDone());
+    p32x_sh2_poll_event(msh2.poll_addr, &msh2, SH2_IDLE_STATES, Pico.t.m68c_aim);
+    p32x_sh2_poll_event(ssh2.poll_addr, &ssh2, SH2_IDLE_STATES, Pico.t.m68c_aim);
     p32x_pwm_ctl_changed();
     p32x_timers_recalc();
   }
@@ -712,11 +713,11 @@ void Pico32xStateLoaded(int is_early)
 
   if (CYCLES_GE(sh2s[0].m68krcycles_done - Pico.t.m68c_aim, 500) ||
       CYCLES_GE(sh2s[1].m68krcycles_done - Pico.t.m68c_aim, 500))
-    sh2s[0].m68krcycles_done = sh2s[1].m68krcycles_done = SekCyclesDone();
-  p32x_update_irls(NULL, SekCyclesDone());
+    sh2s[0].m68krcycles_done = sh2s[1].m68krcycles_done = Pico.t.m68c_aim;
+  p32x_update_irls(NULL, Pico.t.m68c_aim);
   p32x_timers_recalc();
   p32x_pwm_state_loaded();
-  p32x_run_events(SekCyclesDone());
+  p32x_run_events(Pico.t.m68c_aim);
 
   // TODO wakeup CPUs for now. poll detection stuff must go to the save state!
   p32x_m68k_poll_event(0, -1);

commit ad9105ce35147b047b74dc22e7b2c27c33c59480

32x, fix state load regression in 2f95b81

pico/32x/memory.c
@@ -2539,8 +2539,7 @@ void Pico32xMemStateLoaded(void)
   ssh2.poll_addr = ssh2.poll_cycles = ssh2.poll_cnt = 0;
   memset(sh2_poll_fifo, 0, sizeof(sh2_poll_fifo));
 
-  if (Pico32x.emu_flags & P32XF_DRC_ROM_C)
-    sh2_drc_flush_all();
+  sh2_drc_flush_all();
 }
 
 // vim:shiftwidth=2:ts=2:expandtab